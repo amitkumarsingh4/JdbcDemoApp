cab987680084d2634d556c260d27db1f29f01d76
amitkumarsingh4 <amitsinghkumar1@ip-172-31-69-172.ec2.internal>
2020-09-06 15:56:11 +0000

    jdbcmysql

---
+++ src/com/simplilearn/jdbc/CreateConnection.java
@@ -0,0 +1,51 @@
+package com.simplilearn.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateConnection {
+
+	public static void main(String[] args) throws SQLException {
+	
+		String url = "jdbc:mysql://localhost:3306/ecommercedb";
+		String username="root";
+		String password = "root";
+		
+		Connection conn = null;
+		Statement stm = null;
+		ResultSet rst = null;
+	try { 
+		//1. Register Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		//2. Create Connection
+		conn = DriverManager.getConnection(url,username,password);
+		
+		//3. Create Query
+		String query = "select * from eproduct";
+		
+		//4. Create a statement
+		stm = conn.createStatement();
+		
+		//5. execute query
+		rst = stm.executeQuery(query);
+		
+		while (rst.next()) {
+			System.out.println("---------------------------");
+			System.out.println(rst.getInt(1)  + "  " +rst.getString(2) +"   "+ rst.getDouble(3));				
+		}
+		
+	} catch (Exception e) {
+		
+	} finally {
+		rst.close();
+		stm.close();
+		conn.close();
+	}
+	
+	}
+
+}
